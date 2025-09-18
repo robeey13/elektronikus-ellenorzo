9fe78c283cc79f95cb13a828056ba23c852cab1b
Zoli0217 <113987205+Zoli0217@users.noreply.github.com>
2025-09-18 10:17:07 +0200

    started

---
+++ elektronikus_ellenorzo_1tag/Tanulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elektronikus_ellenorzo
+{
+    public class Tanulo
+    {
+        public string Nev { get; set; }
+        public string SzuletesiHely { get; set; }
+        public DateTime SzuletesiIdo { get; set; }
+        public string AnyjaNeve { get; set; }
+        public string Lakcim { get; set; }
+        public DateTime BeiratkozasIdopontja { get; set; }
+        public string Szak { get; set; }
+        public string Osztaly { get; set; }
+        public bool Kollegista { get; set; }
+        public string KollegiumNev { get; set; }
+    }
+}
